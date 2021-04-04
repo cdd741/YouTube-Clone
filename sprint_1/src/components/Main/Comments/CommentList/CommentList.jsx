d4eb60028ac0cf1fa1fa30f9543a7771e25cef18
Baizhou Chen <b99chen@edu.uwaterloo.ca>
2021-04-04 02:33:56 -0400

    minor bug fix
    Signed-off-by: Baizhou Chen <b99chen@edu.uwaterloo.ca>

--- sprint_1/src/components/Main/Comments/CommentList/CommentList.jsx
+++ sprint_1/src/components/Main/Comments/CommentList/CommentList.jsx
@@ -5,7 +5,7 @@ import Comment from "./Comment/Comment";
 const imgList = [
   "https://emoji.slack-edge.com/T01N11LCR55/surprised-jon/016aed46b077ef79.png",
   "https://emoji.slack-edge.com/T01N11LCR55/nuge-thumbs-up/af2d083ed763c373.jpg",
-  "https://emoji.slack-edge.com/T01N11LCR55/nuge-amazed/27e213d16cb2560d.jpg",
+  "https://ca.slack-edge.com/T01N11LCR55-U01N5D1PV39-8e781d88c7e3-512",
 ];
 
 function CommentList({ getTimePassed, className, comments }) {
