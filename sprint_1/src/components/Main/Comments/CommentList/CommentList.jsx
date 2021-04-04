a9f461c9cf4ca566b1d5911fea6bbbd5bcddec9c
Baizhou Chen <b99chen@edu.uwaterloo.ca>
2021-04-04 07:04:58 -0400

    adding relative units
    Signed-off-by: Baizhou Chen <b99chen@edu.uwaterloo.ca>

--- sprint_1/src/components/Main/Comments/CommentList/CommentList.jsx
+++ sprint_1/src/components/Main/Comments/CommentList/CommentList.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import "./CommentList.scss";
 import Comment from "./Comment/Comment";
 
 const imgList = [
   "https://emoji.slack-edge.com/T01N11LCR55/surprised-jon/016aed46b077ef79.png",
-  "https://emoji.slack-edge.com/T01N11LCR55/nuge-thumbs-up/af2d083ed763c373.jpg",
+  "https://ca.slack-edge.com/T01N11LCR55-U01N5D1LC1M-ff75f5b78e12-512",
   "https://ca.slack-edge.com/T01N11LCR55-U01N5D1PV39-8e781d88c7e3-512",
 ];
 
 function CommentList({ getTimePassed, className, comments }) {
   return (
     <ul className={className}>
-      {comments.map((comment, idx) => (
-        <Comment
-          getTimePassed={getTimePassed}
-          comment={comment}
-          img={imgList[idx]}
-          key={comment.id}
-        />
-      ))}
+      {comments
+        .sort((a, b) => b.timestamp - a.timestamp)
+        .map((comment, idx) => (
+          <Comment
+            getTimePassed={getTimePassed}
+            comment={comment}
+            img={imgList[idx]}
+            key={comment.id}
+          />
+        ))}
     </ul>
   );
 }
